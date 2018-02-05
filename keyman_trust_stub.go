df579d7924295c6fe4b58ca33d649e54de5bd53f
Ox Cart <ox.to.a.cart@gmail.com>
2018-02-05 15:27:49 -0600

    Trustdemo includes example of using mshta to prompt on Windows

--- keyman_trust_stub.go
+++ keyman_trust_stub.go
@@ -6,14 +6,14 @@ import (
 	"fmt"
 )
 
-func DeleteTrustedRootByName(commonName string, prompt string) error {
+func DeleteTrustedRootByName(commonName string, prompt string, additionalPrompt string) error {
 	return fmt.Errorf("DeleteTrustedRootByName is not supported on this platform")
 }
 
 // AddAsTrustedRoot adds the certificate to the user's trust store as a trusted
 // root CA. If prompt is provided, privilege escalation will be requested (if
 // required) and the user will be prompted with the given text.
-func (cert *Certificate) AddAsTrustedRoot(prompt string) error {
+func (cert *Certificate) AddAsTrustedRoot(prompt string, additionalPrompt string) error {
 	return fmt.Errorf("AddToUserTrustStore is not supported on this platform")
 }
 
